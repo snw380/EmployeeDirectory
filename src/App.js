187cea6073f2677e871a92327e4793f1fdd50dce
snw380 <ty.morgan818@gmail.com>
2020-11-19 12:35:09 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import Home from "./pages/Home";
+import './App.css';
+
+function App() {
+  return (
+    <div><Home/></div>
+  );
+};
+
+export default App;
---
+++ src/pages/Home.js
@@ -0,0 +1,10 @@
+import React from "react";
+import EmployeePage from "../components/EmployeePage";
+
+function Home() {
+    return (
+        <EmployeePage/>
+    )
+};
+
+export default Home;
