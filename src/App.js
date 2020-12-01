be98eae19fd3d511f5e48f42261b59957e8fa083
snw380 <ty.morgan818@gmail.com>
2020-11-30 21:01:34 -0500

    heroku fix

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import './App.css';
-import Home from "./pages/Home"
+// import React from 'react';
+// import './App.css';
+// import Home from "./pages/Home"
 
-function App() {
-  <Home/>
-}
+// function App() {
+//   <Home/>
+// }
 
-export default App;
+// export default App;
